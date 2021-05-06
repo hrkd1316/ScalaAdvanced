bfcd97508091de721f4860b45a2cc039eb0e1c87
cw-hiroki.kudo <cw-hiroki.kudo@cw-hirokikudonoMacBook-Pro.local>
2021-05-06 12:39:38 +0900

    Add answer of chapter1

--- scala-fs/src/main/scala/Main.scala
+++ scala-fs/src/main/scala/Main.scala
@@ -17,6 +17,15 @@ object Main {
   def fact(n: Int, acc: Int): Int = if (n <= 1) acc else fact(n - 1, acc * n)
 
   /**
+   * 01章中級課題
+   */
+
+  case class Switch(isOn: Boolean)
+  def toggle(switch: Switch): Switch = {
+    if (switch.isOn) Switch(false) else Switch(true)
+  }
+
+  /**
    * 01章上級課題
    */
   def twice(f: Int => Int): Int => Int = (x) => f(f(x))
