3c42d31eb04c14df2c51ce0c02c6419501bf3385
cw-hiroki.kudo <cw-hiroki.kudo@cw-hirokikudonoMacBook-Pro.local>
2021-05-06 15:18:05 +0900

    Add answer of chapter3

--- scala-fs/src/main/scala/Main.scala
+++ scala-fs/src/main/scala/Main.scala
@@ -49,14 +49,47 @@ object Main {
   /**
    * 02章上級課題　模範回答
    */
-  @tailrec
-  def isSorted[E](sortedSeq: Seq[E])(isOrdered: (E, E) => Boolean): Boolean = {
+  def isSorted2[E](sortedSeq: Seq[E])(isOrdered: (E, E) => Boolean): Boolean = {
+    @tailrec
     def isSortedRecursive(i: Int): Boolean = {
       if (i == sortedSeq.length - 1) true
       else if (!isOrdered(sortedSeq(i), sortedSeq(i + 1))) false
       else isSortedRecursive(i + 1)
     }
     if(sortedSeq.length == 0) true else isSortedRecursive(0)
+  }
+
+  /**
+   * 03章初級課題
+   */
+  def syokyuKadai03(): Option[Int] = {
+    val v1: Option[Int] = Some(2)
+    val v2: Option[Int] = Some(3)
+    val v3: Option[Int] = Some(5)
+    val v4: Option[Int] = Some(7)
+    val v5: Option[Int] = Some(11)
+
+    for {
+      i1 <- v1
+      i2 <- v2
+      i3 <- v3
+      i4 <- v4
+      i5 <- v5
+    } yield  i1 * i2 * i3 * i4 * i5
+  }
+
+  /**
+   * 03章中級課題
+   */
+  def chukyuKadai03(x: Int): Option[Int] = {
+    val f1: Option[Int => Int] = Some((x) => x * 2)
+    val f2: Option[Int => Int] = Some((x) => x + 10)
+    val f3: Option[Int => Int] = Some((x) => x / 3)
+    for {
+      i1 <- f1
+      i2 <- f2
+      i3 <- f3
+    } yield i3(i2(i1(x)))
   }
 }
 
